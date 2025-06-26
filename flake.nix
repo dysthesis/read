dcc3804eb415faadf04a0418047e6c89ad01584a
Dysthesis <antheoraviel@protonmail.com>
2025-06-26 18:18:58 +1000

    Flake changes

--- flake.nix
+++ flake.nix
@@ -49,7 +49,7 @@
     treefmt = forAllSystems (pkgs: treefmt-nix.lib.evalModule pkgs ./nix/formatters);
   in
     # Budget flake-parts
-    mapAttrs (_: val: forAllSystems val) {
+    mapAttrs (_: forAllSystems) {
       devShells = pkgs: {default = import ./nix/shell {inherit pkgs self;};};
       # for `nix fmt`
       formatter = pkgs: treefmt.${pkgs.system}.config.build.wrapper;
